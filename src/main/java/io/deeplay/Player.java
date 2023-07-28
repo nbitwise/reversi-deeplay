aafdd57d08c8f3c6f50d3d4aaef5521fee6b03b7
Руслан Кальчевский <rusya754@gmail.com>
2023-07-28 11:52:15 +0600

    feat: добавлена логика доски Добавлена логика и тесты на доску.

--- src/main/java/io/deeplay/Player.java
+++ src/main/java/io/deeplay/Player.java
@@ -1,15 +1,15 @@
 package io.deeplay;
 
 abstract class Player {
-    protected Disk colorOfPlayersDisk; //заменил с чара на диск
+    protected Cell colorOfPlayersCell; //заменил с чара на диск
 
-    public Player(Disk disk) {
-        this.colorOfPlayersDisk = disk;
+    public Player(Cell cell) {
+        this.colorOfPlayersCell = cell;
     }
 
     public abstract int[] makeMove();
 
-    public Disk getOpponent() {
-        return (colorOfPlayersDisk == Disk.BLACK) ? Disk.WHITE : Disk.BLACK;
+    public Cell getOpponent() {
+        return (colorOfPlayersCell == Cell.BLACK) ? Cell.WHITE : Cell.BLACK;
     }
 }
--- src/test/java/io/deeplay/BoardTest.java
+++ src/test/java/io/deeplay/BoardTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
-    Board board = new Board(8);
+    Board board = new Board();
 
     @Test
     void checkNewBoard(){
