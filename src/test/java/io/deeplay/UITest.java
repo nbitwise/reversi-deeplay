4e24c647e5b017ef847f48c8b2e9b0d24eab96c0
User <aleks_alex_antonov@mail.ru>
2023-08-02 15:46:43 +0700

    feat: Замена переменных на финальные

--- src/test/java/io/deeplay/UITest.java
+++ src/test/java/io/deeplay/UITest.java
@@ -3,14 +3,14 @@ package io.deeplay;
 import org.junit.jupiter.api.Test;
 
 
-final class UITest {
+public class UITest {
 
     @Test
     void testDisplayMove() {
-        Board board = new Board();
-        Player player = new Player.BotPlayer(Cell.BLACK); // Создаем игрока для теста
-        Move move = new Move(3, 2); // Создаем ход для теста
-        int moveNumber = 1;
+        final Board board = new Board();
+        final Player player = new Player.BotPlayer(Cell.BLACK); // Создаем игрока для теста
+        final Move move = new Move(3, 2); // Создаем ход для теста
+        final int moveNumber = 1;
         UI.displayMove(moveNumber, board, player, move); // Вывод состояния доски после хода в консоль
     }
 }
