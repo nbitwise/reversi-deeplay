f6314d5cb8a7c104d046ff7e36471d657cb85a58
Руслан Кальчевский <rusya754@gmail.com>
2023-08-02 11:50:26 +0600

    fix: пофикшены баги с использованием парсера и логики завершения игры

--- src/test/java/io/deeplay/GameTest.java
+++ src/test/java/io/deeplay/GameTest.java
@@ -18,7 +18,7 @@ public class GameTest {
                 "w b b b b b w b \n" +
                 "w b w b b b w b \n" +
                 "w w w w w b w b \n" +
-                "w w w b b b b b", 'b', 'w');
+                "w w w b b b b b", 'b', 'w','_');
         Board preFinalBoard = BoardParser.parse("b b b b b b w   \n" +
                 "w b w w w b w w \n" +
                 "w w b b w w w w \n" +
@@ -26,7 +26,7 @@ public class GameTest {
                 "w b b b b b w w \n" +
                 "w b w b b b w w \n" +
                 "w w w w w b w w \n" +
-                "w w w b b b b b ", 'b', 'w');
+                "w w w b b b b b ", 'b', 'w','_');
         Game.startGame(preFinalBoard, new Player.BotPlayer(Cell.BLACK), new Player.BotPlayer(Cell.WHITE));
         assertEquals(finalBoard, preFinalBoard);
     }
