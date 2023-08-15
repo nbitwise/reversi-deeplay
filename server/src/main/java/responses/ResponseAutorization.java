acaea20ee5340d54dcb3254f8b91cadd9305f609
lpycukl <44721183+lpycukl@users.noreply.github.com>
2023-08-15 14:33:55 +0600

    Delete ResponseAutorization.java
    дубликат, еще и название неверное

--- server/src/main/java/responses/ResponseAutorization.java
+++
@@ -1,9 +0,0 @@
-package responses;
-
-public class ResponseAutorization extends Response {
-
-    public ResponseAutorization(String status, String message) {
-        super(status, message);
-
-    }
-}
