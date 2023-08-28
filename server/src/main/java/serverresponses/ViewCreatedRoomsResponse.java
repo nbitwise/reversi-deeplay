c800c4d429f593b1f153de818db1ba467d4bbdb1
Ilya Istomin <ist_ia@mail.ru>
2023-08-28 12:03:10 +0700

    feat: добавлен класс клинета  бота

---
+++ server/src/main/java/serverresponses/ViewCreatedRoomsResponse.java
@@ -0,0 +1,18 @@
+package serverresponses;
+
+
+public class ViewCreatedRoomsResponse implements Response {
+        public final String command = "VIEWROOMS";
+
+        public int roomId;
+
+        public String status;
+
+        public String message;
+
+        public ViewCreatedRoomsResponse(String status, String message, int roomId) {
+                this.message = message;
+                this.roomId = roomId;
+                this.status = status;
+        }
+}
