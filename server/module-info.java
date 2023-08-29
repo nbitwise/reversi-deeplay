717acbec384f507a25a00469eb0a1e2936f702f1
lpycukl <44721183+lpycukl@users.noreply.github.com>
2023-08-29 12:26:42 +0600

    add bot to server

--- server/module-info.java
+++ server/module-info.java
@@ -1,7 +1,9 @@
 module reversi.deeplay.server.main {
     requires com.google.gson;
     requires reversi.deeplay.gamelogic.main;
+    requires reversi.deeplay.localGame.main;
+    requires java.desktop;
+
     exports serverresponses;
-    exports serserrequest;
-    requires com.example.client;
+    exports serverrequest;
 }
